4cdee1fc0f2412492d979288cc19fe625d8804db
Chentian Jiang <chentian.jiang@gmail.com>
2020-11-18 12:02:33 +0000

    fix image path for netlify proxy

--- src/components/End.svelte
+++ src/components/End.svelte
@@ -7,7 +7,7 @@
 <CenteredCard has_button={false}>
     <h3 style="margin-bottom: 0">Thank you for participating!</h3>
     <p style="color: green;">Your final score is: {$current_score}/{$total_score}</p>
-    <img src="/images/thankyou.jpg" alt="dog biting a thank you card">
+    <img src="/active-blicket-comp/images/thankyou.jpg" alt="dog biting a thank you card">
     <span class="attribution"><br/>Photo by <a href="https://unsplash.com/@howier?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Howie R</a> on <a href="https://unsplash.com/s/photos/thank-you?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank">Unsplash</a></span>
 </CenteredCard>
 
