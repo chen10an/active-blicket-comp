4f981553f2762a20d31b0d4399a5e0f047302d86
Chentian Jiang <chentian.jiang@gmail.com>
2021-06-10 12:17:03 +0200

    update quiz instructions

--- src/components/pages/Quiz.svelte
+++ src/components/pages/Quiz.svelte
@@ -200,11 +200,12 @@
         <p><b>Your score (and resulting bonus)</b> will be calculated and shown at the <b>end of the study</b>. Only "Do you think these blocks are blickets?" and "How would you teach others about the blicket machine?" will be scored, but we hope you'll sincerely answer all questions.</p>
 
         <h3>Do you think these blocks are blickets?</h3>
+        <p>Your score and bonus is calculated based on how close your blicket rating is to the correct rating. They will be shown at the <b>end of the study</b>.</p>
         <div class="info-box">
             <p><b>Details about calculating score and bonus:</b></p>
-            <p>Each questions is scored as [1 - (difference between your rating and the correct rating)/10].</p>
+            <p>Each question is scored as [1 - (difference between your rating and the correct rating)/10].</p>
             <p>For example, if the correct rating is 10 and you answer 7, your score is 0.7. If the correct rating is 0 and you answer 3, your score is also 0.7.</p>
-            <p>The scores for all questions are added together and multiplied with {$bonus_currency_str}{$bonus_val} to produce your bonus (up to {$bonus_currency_str}{$bonus_val*$quiz_data_dict[collection_id].blicket_rating_groups.length} in this section.)</p>
+            <p>The scores for the questions here are added together and multiplied with {$bonus_currency_str}{$bonus_val} to produce your bonus (up to {$bonus_currency_str}{$bonus_val*$quiz_data_dict[collection_id].blicket_rating_groups.length}).</p>
         </div>
         
         <!-- Iterate over $block_dict, which orders blocks alphabetically -->
@@ -233,16 +234,17 @@
         <textarea bind:value={$quiz_data_dict[collection_id].free_response_1} disabled="{!hide_correct_answers}"></textarea>
 
         <h3>How would you teach others about the blicket machine?</h3>
-        <p>We are asking you to help us give 5 examples to help other people understand how the machine works. In each example, you can choose to add</p>
-            <div class="block-key"><Block block={new BlockClass(-1, false, "light-gray", "", -1)} is_mini={true} use_transitions="{false}" is_disabled="{true}" /> blickets</div>
-            <p>and</p>
-            <div class="block-key"><Block block={new BlockClass(-1, false, "dark-gray", "", -1)} is_mini={true} use_transitions="{false}" is_disabled="{true}" /> plain blocks (not blickets) </div>
-            <p>to a blicket machine. You can then choose whether that blicket machine should be <span style="background: var(--active-color); padding: 0 0.3rem;">activated</span> or deactivated.</p>
+        <p>We are asking you to help us give 5 examples to help other people understand how the blicket machine works. In each example, you can choose to add</p>
+        <div class="block-key"><Block block={new BlockClass(-1, false, "light-gray", "", -1)} is_mini={true} use_transitions="{false}" is_disabled="{true}" /> blickets</div>
+        <p>and</p>
+        <div class="block-key"><Block block={new BlockClass(-1, false, "dark-gray", "", -1)} is_mini={true} use_transitions="{false}" is_disabled="{true}" /> plain blocks (not blickets) </div>
+        <p>to a blicket machine. You can then choose whether that blicket machine should be <span style="background: var(--active-color); padding: 0 0.3rem;">activated</span> or deactivated.</p>
+        
+        <p>We will show your examples to other people <b>after the study</b>. They will also know that blickets are light gray and plain blocks are dark gray. Your bonus is calculated based on how well they understand the blicket machine. This process may take some time: we will send you your bonus <b>within 1.5 weeks</b>.</p>
 
         <div class="info-box">
-            <p><b>Details about bonus calculations:</b></p>
-            <p>We will show your examples to other people <i>after the study</i>. Your bonus is calculated based on how well they understand the blicket machine. This process can take some time: we will send you your bonus within 1.5 weeks.</p>
-            <p>Two other people will choose from 7 options about how the blicket machine works. If one person chooses the correct option, your bonus for this section is $0.075; if both choose the correct option, your bonus is $0.15.</p>
+            <p><b>Details about calculating bonus:</b></p>
+            <p>Two other people will choose from 7 options about how the blicket machine works. If one person chooses the correct option, your bonus is $0.075; if both choose the correct option, your bonus is $0.15.</p>
         </div>
         
         {#each $quiz_data_dict[collection_id].teaching_ex as ex, i}
